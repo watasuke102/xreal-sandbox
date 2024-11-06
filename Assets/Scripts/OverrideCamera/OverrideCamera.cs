7b4b1307f0a943295b3e564d970ccb8a8245d2e3
watasuke102 <watasuke102@gmail.com>
2024-11-06 15:44:53 +0900

    update: create directory for larger scenes

--- Assets/Scripts/OverrideCamera/OverrideCamera.cs
+++ Assets/Scripts/OverrideCamera/OverrideCamera.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// https://gamedev.stackexchange.com/questions/98632/drawing-a-pixel-to-rendertexture
 public class OverrideCamera : MonoBehaviour
 {
   void OnRenderImage(RenderTexture _, RenderTexture dst)
