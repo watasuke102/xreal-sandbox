573879e3e2a6fdfefeab6ce47a00dcfbc38b72d7
watasuke102 <watasuke102@gmail.com>
2024-11-23 11:01:59 +0900

    remove: opengl-texture
    Unity tries to build these cpp files

--- Assets/Plugins/opengl-texture/build/jni/Application.mk
+++
@@ -1,3 +0,0 @@
-APP_ABI               := arm64-v8a
-APP_PLATFORM          := android-29
-NDK_TOOLCHAIN_VERSION := clang
--- Assets/Plugins/opengl-texture/inc/shader.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <GLES3/gl3.h>
-
-GLuint compile_shader(const char* vertex_src, const char* flagment_src);
