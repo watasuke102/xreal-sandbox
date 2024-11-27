6322f26f4d96c266cf19a3376c2deb0e7c81a036
watasuke102 <watasuke102@gmail.com>
2024-11-27 15:00:00 +0900

    update: DispCameraStatus>show only vp/v/p mat and camera rot

--- Assets/Scripts/DispCameraStatus.cs
+++ Assets/Scripts/DispCameraStatus.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using NRKernal;
 using TMPro;
 using UnityEngine;
 
 public class DispCameraStatus : MonoBehaviour
 {
   [SerializeField] GameObject cameraRig;
+  [SerializeField] bool is_left;
   TextMeshProUGUI label;
+
   void Start()
   {
     this.label = GetComponent<TextMeshProUGUI>();
@@ -14,14 +17,31 @@ public class DispCameraStatus : MonoBehaviour
   void Update()
   {
     var cam = this.cameraRig.GetComponent<Camera>();
-    var m = transform.localToWorldMatrix;
+    // var m = transform.localToWorldMatrix;
     var v = cam.worldToCameraMatrix;
-    var p =
-     GL.GetGPUProjectionMatrix(cam.projectionMatrix, true);
-    var mvp = p * v * m;
-    this.label.text = mvp.ToString() + "\n";
-    this.label.text += "\n" + m.ToString();
-    this.label.text += "\n" + v.ToString();
-    this.label.text += "\n" + p.ToString();
+
+    bool is_succeeded;
+    var proj_list = NRFrame.GetEyeProjectMatrix(out is_succeeded, 0.01f, 100.0f);
+    var p = Matrix4x4.zero;
+    if (is_succeeded)
+    {
+      if (this.is_left)
+      {
+        p = proj_list.LEyeMatrix;
+      }
+      {
+        p = proj_list.REyeMatrix;
+      }
+      p = GL.GetGPUProjectionMatrix(p, true);
+    }
+
+    var vp = p * v;
+    var text = "";
+    text += "[vp]\n" + vp.ToString() + "\n";
+    text += "[v]\n" + v.ToString();
+    text += "[p]\n" + p.ToString();
+    text += "\n" + cam.transform.eulerAngles.ToString();
+    this.label.text = text;
+    Debug.Log(text);
   }
 }
